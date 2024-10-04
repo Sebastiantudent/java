b076efb993b320e5c5d4e1c79634d2a46da4816c
Sebastiantudent <147138818+Sebastiantudent@users.noreply.github.com>
2024-10-04 17:58:52 +0200

    Create zestaw 1.java

---
+++ zestaw 1.java
@@ -0,0 +1,128 @@
+package javaTpoint.MicrosoftJava; 
+package java.time
+/*
+class zad1 {
+    public static void main(String[] args) {
+        System.out.println("Sebastian Sliski");
+    }
+}
+*/
+/*
+class zad2 {
+    String imie = "sebastian";
+    
+    String nazwisko = "Sliski";
+    
+    public static void zad2_1(String[] args) {
+        zad2 im = new zad2();
+    System.out.println(im.imie.length());
+    System.out.println(im.nazwisko.length());
+    }
+}
+*/
+/*
+class zad3 {
+    String A = "Ala ";
+    String B = "ma ";
+    String C = "kota";
+    String D = A+B+C;
+    public static void main(String[] args) {
+        zad3 wys = new zad3();
+        System.out.println(wys.D);
+    }
+}
+*/
+/*
+class zad4_1 {
+    public static void main(String[] args) {
+        System.out.println("                      *");
+        System.out.println("                      * *");
+        System.out.println("                      * * *");
+        System.out.println("* * * * * * * * * * * * * * *");
+        System.out.println("* * * * * * * * * * * * * * * *");
+        System.out.println("* * * * * * * * * * * * * * *");
+        System.out.println("                      * * *");
+        System.out.println("                      * *");
+        System.out.println("                      *");
+        
+    }
+}
+*/
+/*
+class zad4_2 {
+    public static void main(String[] args) {
+        System.out.println("     *");
+        System.out.println("    * *");
+        System.out.println("   * * *");
+        System.out.println("  * * * *");
+        System.out.println(" * * * * *");
+        System.out.println("* * * * * *");
+        System.out.println(" * * * * *");
+        System.out.println("  * * * *");
+        System.out.println("   * * *");
+        System.out.println("    * *");
+        System.out.println("     *");
+    }
+}
+*/
+/*
+class zad5{
+    String A = "Ala ma kota";
+     public static void main(String[] args) {
+         
+     }
+}
+*/
+/*
+class zad6{
+    String A = "Ala ma kota";
+    
+     public static void main(String[] args) {
+         zad6 duze = new zad6();
+         String H = "";
+         duze.A = H;
+         System.out.println(H.toUpperCase());
+     }
+}
+zad 7
+A = 65 !=33 @=64 >=62 ~=126 \n=10 
+zad 8 
+liczby od 48 do 57
+duze litery od 65 do 90
+male litery od 97 do 122
+*/
+/*
+class zad9
+{
+    static void wstaw(String fname, String gname, String hname){
+        System.out.println("Grzesiek nie wiedzial dlaczego" + fname + "jest tak drapieznym" + gname + "mimo ze jego" + hname + "na to nie wskazuje");
+    }
+    public static void main(String[] args) {
+         wstaw(" Adam "," kotem "," postawa ");
+     }
+}
+*/
+/*
+class zad10
+{
+    static void wstaw(String fname, String gname){
+        System.out.println(fname + " to najlepsza ksiazka napisana przez "+ gname);
+    }
+    public static void main(String[] args) {
+         wstaw("Wiedzmin","Sapkowskiego");
+     }
+}
+*/
+/*
+class zad11 {
+    int i = 0;
+    public static void main(String[] args) {
+        zad11 wypisz = new zad11();
+        while(wypisz.i<5){
+            System.out.print("Wodrze ");
+            wypisz.i++;
+        }
+        
+    }
+}
+*/
