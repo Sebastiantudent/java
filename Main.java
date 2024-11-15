6cef308ecf11938cd76a6d49cd02c479ebe52e15
Sebastiantudent <147138818+Sebastiantudent@users.noreply.github.com>
2024-11-15 17:52:44 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,19 +2,16 @@ import java.lang.Math;
 public class Main {
     public static int[] ciagArytmetycznyRodzajuM(int n, int m, int a1, int[] r){
         int[] tab = new int[n];
-        int j=1;
-        tab[0]=a1;
-        for(int i = 0; i < m; i++) {
-            if (i == 0) {
-               // tab[i]=a1;
+        tab[0] = a1;
+        for(int i = 1; i < n; i++){
+            int suma=a1;
+            for(int j = 0; j < m; j++){
+                suma+=Math.pow(i,j+1)*r[j];
+
             }
-
-            //tab[i] +=Math.pow((n - 1), j) * Math.pow(r[i], j);
-            j++;
+            tab[i] = suma;
         }
-
         return tab;
-        //an = a1+(n-1)*r+(n-1)2 * r2...
     }
     public static void wypiszTablice(int[] tab,int n) {
 
@@ -35,20 +32,24 @@ public class Main {
            }
 
         }
+
         return true;
     }
-    /*
+
     public static boolean czyciagArytmetycznyRodzajuM(int[] tab,int m){
-        int roznica = m-1;
-        int[] tab2 = new int[tab.length-roznica];
-        for(int i = 0; i<tab.length; i++){
-
-
+        for(int i = 0; i < m-1; i++){
+            for(int j = 0; j < tab.length-1-i; j++){
+                tab[j]=tab[j+1]-tab[j];
             }
 
-
+        }
+        int [] wynik= new int[tab.length-(m-1)];
+        for(int i = 0; i < wynik.length; i++){
+            wynik[i]=tab[i];
+        }
+    return czyciagArytmetyczny(wynik);
     }
-    */
+
     public static int podciag(int[] tab,int r){
         int k = 1;
         int m = k;
@@ -99,34 +100,27 @@ public class Main {
     }
     public static void sekwencjaCollatza(int n,int c){
         int k = c;
-        for(int i = 0; i<n; i++){
+        String Wynik = ""+c;
+        for(int i = 0; i<n-1; i++){
             if(k % 2==0) {
                 k = k /2;
-                System.out.print(k + " ");
+                Wynik += " "+k;
             }
             else if(k % 2 != 0) {
-                k = k*3-1;
-                System.out.print(k + " ");
+                k = k*3+1;
+                Wynik += " "+k;
             }
         }
+        System.out.println(Wynik);
     }
     public static void minmaxsekwencjaCollatza(int n,int c){
         int k = c;
         int m = 0;
         int min = c;
         int max = c;
-        if(k % 2==0){
-            m = k/2;
-            min = m;
-            max = m;
-        }
-        else {
-            m = k*3-1;
-            min = m;
-            max = m;
-        }
 
-        for(int i = 0; i<n; i++){
+
+        for(int i = 0; i<n-1; i++){
             if(k % 2==0) {
                 k = k / 2;
                 if(max < k ){
@@ -138,7 +132,7 @@ public class Main {
 
             }
             else if(k % 2 != 0) {
-                k = k*3-1;
+                k = k*3+1;
                 if(max < k){
                     max = k;
                 }
@@ -152,16 +146,46 @@ public class Main {
         System.out.println("min="+min);
         System.out.println("max="+max);
     }
+    public static boolean zad1_2(double x, double y, int k){
+        return Math.abs(x-y)<=Math.pow(10,-k);
+    }
+    public static int zad2_2(int S){
+        int i = 0;
+        while (!(Math.pow(i,2)<S && Math.pow(i+1,2)>S)) {
+            i++;
+        }
+        return i;
+    }
+
+    public static double zad3_2(int S, int n, int k){
+        double x = zad2_2(S);
+        double x_next = ((n-1)*x+(S/Math.pow(x,n-1)))/n;
+        while (zad1_2(x_next,x,k)){
+        x = x_next;
+        x_next = ((n-1)*x+(S/Math.pow(x,n-1)))/n;
+        }
+        return x_next;
+    }
+
     public static void main(String[] args) {
 
-        //wypiszTablice(ciagArytmetycznyRodzajuM(3,3,1,new int[]{1,2,3}),3);
+
+        System.out.println("=========================================");
+        int tab1[] = ciagArytmetycznyRodzajuM(10,1,3,new int[]{2});
+        for (int i = 0; i < tab1.length; i++) {
+
+            System.out.print(tab1[i] + " ");
+
+        }
         System.out.println();
-        int [] tab = {10,20,30,40,50};
+        int [] tab = {1,2,0,3,5,1,4};
         System.out.println(czyciagArytmetyczny(tab));
         System.out.println(podciag(tab));
-        sekwencjaCollatza(6,5);
+
+
+        sekwencjaCollatza(10,6);
         System.out.println();
-        minmaxsekwencjaCollatza(1,5);
+        minmaxsekwencjaCollatza(10,6);
         System.out.println(podciag(tab,10));
 
     }
