5657cecab7a4920becdbb660d228942a0cc207a7
Sebastiantudent <147138818+Sebastiantudent@users.noreply.github.com>
2025-01-17 17:15:14 +0100

    Zestaw12

--- Main.java
+++ Main.java
@@ -1,38 +1,93 @@
-import java.util.ArrayList;
-import java.util.Objects;
+import java.util.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public void fuelCost(double fulPrice, double distance){
 
+    public static <T> void printUnique(Collection<T> items) {
+        Set<T> uniqueElements = new HashSet<>(items);
+
+        for (T item : uniqueElements) {
+            System.out.println(item + " ");
+        }
     }
+    public static <T> int countOccurrences(Collection<T> items,T element) {
+        int count = 0;
+        for (T item : items) {
+            if (item.equals(element)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static <T>ArrayList<T> mergeLists(ArrayList <T> items1, ArrayList<T> Items2) {
+        ArrayList<T> mergedItems = new ArrayList<>();
+        mergedItems.addAll(items1);
+        mergedItems.addAll(Items2);
+        return mergedItems;
+    }
+    public static <T> int countElements(Iterable<T> items, T element){
+        int count = 0;
+        for (T item : items) {
+            if (item.equals(element)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static <T> boolean isPalindrome(LinkedList <T> items) {
+        LinkedList <T> temp = items.reversed();
+        if(temp.equals(items)){
+            return true;
+        }
+        else
+            return false;
+    }
+    public static <T> HashSet<T> findUnique(List <T> items) {
+    HashSet <T> uniqueItems = new HashSet<>();
+    for (T item : items) {
+        uniqueItems.add(item);
+    }
+    return uniqueItems;
+    }
+    //F1
+    public static <T> TreeSet<T> findElementsinRange(TreeSet<T> items, T lowerBound, T upperBound) {
+        TreeSet<T> mergedItems = (TreeSet<T>) items.subSet(lowerBound,true,upperBound,true);
+        return mergedItems;
+    }
+    public static <T,V> HashMap<V,T> swapKeysAndValues(HashMap<T,V> map) {
+        HashMap<V,T> countOccurrences = new HashMap<>();
+        for (T item : map.keySet()) {
+        countOccurrences.put(map.get(item), item);
+        }
+        return countOccurrences;
+    }
+    public static <T,V> TreeMap<T,V> subMap(TreeMap<T,V> map, T lower, T upper) {
+        TreeMap<T,V> sortedMap = (TreeMap<T,V>) map.subMap(lower, upper);
+        //sortedMap.subMap(lower, true, upper, true);
+        return sortedMap;
+    }
+
     public static void main(String[] args) {
-    ArrayList<Double> oceny = new ArrayList<Double>();
-    oceny.add(1.0);
-    oceny.add(2.0);
-    oceny.add(3.0);
-    oceny.add(4.0);
-    MusicAlbum PolskiJazz = new MusicAlbum("Jazz","Jake Balboa", oceny);
-    RockAlbum RockowyJazz = new RockAlbum("Jazz","Jake Balboa", oceny,"rock and Jazz");
-    PolskiJazz.setArtist("Pogba");
+    TreeSet <Integer> items = new TreeSet<>();
+    items.add(1);
+    items.add(2);
+    items.add(3);
+    items.add(4);
+    items.add(5);
+    items.add(6);
+    items.add(7);
+    System.out.println(findElementsinRange(items,1,3));
+    LinkedList <Integer> items1 = new LinkedList <>();
+    items1.add(1);
+    items1.add(2);
+    items1.add(3);
+    items1.add(4);
+    items1.add(3);
+    items1.add(2);
+    items1.add(1);
+    items1.add(5);
+    System.out.println(isPalindrome(items1));
 
-    PolskiJazz.addRatings(3.0);
-    PolskiJazz.toString();
-    PolskiJazz.removeRatings(4);
-    RockowyJazz.setRockgenre("Rock and Roll");
 
-    record BookDTO(String title, String author, Double price, String genre) {}
-        BookDTO fantasy = new BookDTO("Wiedzmin", "Sapkowski", 17.00,"fanstasy");
-        BookDTO Science_fiction = new BookDTO("Avatar", "ktos", 18.00,"Science Fiction");
 
-       record Address(String street,int housenumber, String city, String Postalcode) {}
-        record Osoba(Address address,String name,String Lastname) {}
-        record Car(String brand, String model, double fuelConsuptionPer100km) {}
-        Car car = new Car("Bmw","h1t",73.70);// 73.70/100 = 1km/koszt
-
-       // RockowyJazz.equals(PolskiJazz);
     }
-
-
 }
