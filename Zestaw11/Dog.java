3252ebeb37d68c8146921e5442f258ad22a134a8
Sebastiantudent <147138818+Sebastiantudent@users.noreply.github.com>
2025-01-17 12:13:38 +0100

    Update Dog.java

--- Zestaw11/Dog.java
+++ Zestaw11/Dog.java
@@ -1,4 +1,16 @@
-public class Dog extends Animal {
+class Animal implements Comparable<Animal>{
+    public int wiek;
 
-
+    public Animal(int wiek) {
+        this.wiek = wiek;
+    }
+    @Override
+    public int compareTo(Animal other){
+        return wiek - other.wiek;
+    }
+}
+class Dog extends Animal{
+    public Dog(int wiek) {
+        super(wiek);
+    }
 }
